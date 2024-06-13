ffcdd3ca5e437ea0141d0c136bfe7b9914c2e3d5
sangwan kim <sangwan@kisti.re.kr>
2024-06-13 00:20:01 +0000

    no message

--- simd/amxtile.c
+++ simd/amxtile.c
@@ -1,27 +1,32 @@
 // amxtile.c 
 #include <immintrin.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <stdio.h>
-#include <immintrin.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <sys/syscall.h>
 #include <unistd.h>
 #include <stdbool.h>
 #include "rdtsc.h"
 
-#define MAX 1024
+#ifndef MAX_ROWS
 #define MAX_ROWS 16
+#endif
+#ifndef MAX_COLS
 #define MAX_COLS 64
+#endif
+#ifndef MAX
+#define MAX 1024 // MAX_ROWS x MAX_COLS
+#endif
+#ifndef STRIDE
 #define STRIDE 64
+#endif
+
 #define ARCH_GET_XCOMP_PERM     0x1022
 #define ARCH_REQ_XCOMP_PERM     0x1023
 #define XFEATURE_XTILECFG       17
 #define XFEATURE_XTILEDATA      18
 // Define tile config data structure 
-typedef struct __tile_config
-{
+typedef struct __tile_config {
   uint8_t palette_id;
   uint8_t start_row;
   uint8_t reserved_0[14];
@@ -33,24 +38,28 @@ static void init_tile_config (__tilecfg *tileinfo) {
   int i;
   tileinfo->palette_id = 1;
   tileinfo->start_row = 0;
-  for (i = 0; i < 1; ++i) {
+  for (i = 0; i < 1; i++) {
     tileinfo->colsb[i] = MAX_ROWS;
     tileinfo->rows[i] =  MAX_ROWS;
   }
-  for (i = 1; i < 4; ++i) {
+  for (i = 1; i < 4; i++) {
     tileinfo->colsb[i] = MAX_COLS;
     tileinfo->rows[i] =  MAX_ROWS;
   }
   _tile_loadconfig (tileinfo);
 }
 // Initialize int8_t buffer 
-static void init_buffer (int8_t *buf, int8_t value) {
+static void init_buffer (int8_t *buf, int8_t v1, int8_t v2) {
   int rows, colsb, i, j;
+  int8_t v;
   rows  = MAX_ROWS;
   colsb = MAX_COLS;
-  for (i = 0; i < rows; i++)
-    for (j = 0; j < colsb; j++)
-        buf[i * colsb + j] = value;
+  for (i = 0; i < rows; i++) {
+    for (j = 0; j < colsb; j++) {
+      if (j < colsb/2) v = v1; else v = v2;
+      buf[i * colsb + j] = v;
+    }
+  }
 }
 // Initialize int32_t buffer 
 static void init_buffer32 (int32_t *buf, int32_t value) {
@@ -88,7 +97,7 @@ static void print_buffer(int8_t* buf, int32_t rows, int32_t colsb) {
 static void print_buffer32(int32_t* buf, int32_t rows, int32_t colsb) {
   printf("print_buffer32 rows=%d colsb=%d\n", rows, colsb);
   for (int i = 0; i < rows; i++)  {
-    for (int j = 0; j < (colsb); j++) {
+    for (int j = 0; j < colsb; j++) {
        printf("%d ", buf[i * colsb + j]);
     }
     printf("\n");
@@ -109,16 +118,11 @@ int main() {
   // Request permission to linux kernel to run AMX 
   if (!set_tiledata_use()) exit(-1);
 
-  // Load tile configuration 
-  init_tile_config (&tile_data);
+  init_tile_config (&tile_data); // Load tile configuration 
 
-  // Init src matrix buffers with data
-  init_buffer (src1, 2);
-  print_buffer(src1, rows, colsb);
-  init_buffer (src2, 2);
-  print_buffer(src2, rows, colsb);
-  // Init dst matrix buffers with data
-  init_buffer32 (res, 0);
+  init_buffer (src1, 0, 1); print_buffer(src1, rows, colsb);
+  init_buffer (src2, 0, 2); print_buffer(src2, rows, colsb);
+  init_buffer32 (res, 100); print_buffer32(res, rows, colsb/4);
 
   // Load tile rows from memory
   _tile_loadd (2, src1, STRIDE);
@@ -132,11 +136,8 @@ int main() {
   elapsed = end - start;
   printf("tile_dpbssd Elapsed Time (cycles): %llu\n", elapsed);
 
-  // Store the tile data to memory
   _tile_stored (1, res, STRIDE);
   print_buffer32(res, rows, colsb/4);
-  // Release the tile configuration to return to the init state, 
-  // which releases all storage it currently holds
   _tile_release ();
 }
 
