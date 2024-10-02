796133a27d7c49bab689b42b5ca7734f1f9ffda3
Ubuntu <ubuntu@ip-172-31-15-63.ap-northeast-2.compute.internal>
2024-10-02 02:12:14 +0000

    no message

---
+++ cmake/Step8/MathFunctions/MakeTable.cmake
@@ -0,0 +1,13 @@
+
+# first we add the executable that generates the table
+add_executable(MakeTable  MakeTable.cxx)
+target_link_libraries(MakeTable PRIVATE  tutorial_compiler_flags)
+
+# add the command to generate the source code
+# MakeTable 프로그램을 이용하여 소스코드를 생성한다. 
+add_custom_command(
+  OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/Table.h
+  COMMAND MakeTable ${CMAKE_CURRENT_BINARY_DIR}/Table.h
+  DEPENDS MakeTable
+  )
+
