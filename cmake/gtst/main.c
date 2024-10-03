5c26cb2050eff8a2ea4c4488cbd5a803b4844a2a
Ubuntu <ubuntu@ip-172-31-15-63.ap-northeast-2.compute.internal>
2024-10-03 15:11:40 +0000

    no message

---
+++ cmake/gtst/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "sum.h"
+
+int main() {
+  printf("sum : %d\n", sum(1, 1));
+  return 0;
+}
---
+++ cmake/gtst/sum_test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <gtest/gtest.h>
+
+#include "sum.h"
+
+TEST(sum_test1, test1) {
+  EXPECT_EQ(2, sum(1,1));
+  EXPECT_EQ(0, sum(1,-1));
+}
+TEST(sum_test2, test2) {
+  EXPECT_EQ(100, sum(50,50));
+}
+
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
